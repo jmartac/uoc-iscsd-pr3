3da676e4dec5145492ad04f397de037c12627b2c
Javi <javiermartac@gmail.com>
2022-05-25 13:07:22 +0100

    ShowUnitTest

---
+++ src/test/java/edu/uoc/epcsd/showcatalog/ShowUnitTest.java
@@ -0,0 +1,21 @@
+package edu.uoc.epcsd.showcatalog;
+
+import edu.uoc.epcsd.showcatalog.domain.Show;
+import edu.uoc.epcsd.showcatalog.domain.Status;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ShowUnitTest {
+
+    @Test
+    public void givenShow_whenCancelled_thenShowStatusIsCancelled() {
+        Show show = new Show();
+        show.cancel();
+
+        assertThat(show.getStatus())
+                .isEqualTo(Status.CANCELLED);
+    }
+
+}
